e40c85b262859cefc3376133050e46121cba7f1f
Aaron Ortbals <aaron.ortbals@gmail.com>
2013-05-21 13:43:01 -0500

    added process and validate to basic

--- vendor/assets/javascripts/jquery-fileupload/basic.js
+++ vendor/assets/javascripts/jquery-fileupload/basic.js
@@ -1,4 +1,6 @@
 //=require jquery-fileupload/vendor/jquery.ui.widget
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
+//=require jquery-fileupload/jquery.fileupload-process
+//=require jquery-fileupload/jquery.fileupload-validate
 
