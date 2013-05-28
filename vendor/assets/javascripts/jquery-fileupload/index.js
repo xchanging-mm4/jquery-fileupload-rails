1f4a251e0fc71fb3a2a1a28df01fca535c524981
Aaron Ortbals <aaron.ortbals@gmail.com>
2013-05-28 11:34:17 -0500

    added updated versions of canvas-to-blob and load-image, updated index.js

--- vendor/assets/javascripts/jquery-fileupload/index.js
+++ vendor/assets/javascripts/jquery-fileupload/index.js
@@ -4,6 +4,8 @@
 //=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
-//=require jquery-fileupload/jquery.fileupload-fp
+//=require jquery-fileupload/jquery.fileupload-process
+//=require jquery-fileupload/jquery.fileupload-validate
+//=require jquery-fileupload/jquery.fileupload-resize
 //=require jquery-fileupload/jquery.fileupload-ui
 //=require jquery-fileupload/locale
